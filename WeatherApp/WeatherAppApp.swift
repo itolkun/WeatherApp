1662d75ad3c927189a260ba5e0cdd44c8930accf
 Aitolkun Anarbekova <tokusha.aa@gmail.com>
2023-12-13 15:57:29 +0300

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Айтолкун Анарбекова on 13.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
